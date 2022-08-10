f040bd5af6d83abcbc6a3c0a657174f75d43cb65
CalvinParkNov <dev.calvinpark@gmail.com>
2022-08-10 09:46:52 +0900

    gitignroe added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("welcome");
 });
-app.listen(PORT, () => {
+app.listen(PORT || 6000, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
 });
