05e5d1975dda9eb4ebc5a36f06206fd455c5f5b0
swh_95 <tjdngur22@gmail.com>
2022-08-10 09:41:49 +0900

    start
    start

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
+});
