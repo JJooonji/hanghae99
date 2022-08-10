e11f1d540f4a468638f5e6ab0541cea66dbaa924
CalvinParkNov <dev.calvinpark@gmail.com>
2022-08-10 10:24:58 +0900

    test get complete

--- app.test.js
+++ app.test.js
@@ -1,1 +1,23 @@
+const { json } = require("express");
 const request = require("supertest");
+const app = require("./app");
+
+describe("Testing API ", () => {
+  it("GET /api", () => {
+    return request(app)
+      .get("/api")
+      .expect("Content-type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              id: expect.any(Number),
+              title: expect.any(String),
+              nickname: expect.any(String),
+            }),
+          ])
+        );
+      });
+  });
+});
